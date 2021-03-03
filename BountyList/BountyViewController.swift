2ab002c2d680da1f840ef3f0b323aec53c465421
Woozzang <woozzaang@gmail.com>
2021-03-04 01:21:57 +0900

    Set datasource & delegate of bountytableview

--- BountyList/BountyViewController.swift
+++ BountyList/BountyViewController.swift
@@ -10,20 +10,29 @@ import UIKit
 class BountyViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-
+      super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+  
+}
+
+// MARK: UITableViewDataSource
+extension BountyViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 2
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
     
+    return cell
+  }
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+// Mark: UITableViewDelegate
+extension BountyViewController: UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print("---> \(indexPath.row)")
+  }
 }
